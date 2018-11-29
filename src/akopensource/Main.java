cde1a5612264fbe9a77fa09e71f3bedbd8508a8a
AhmedKhaledAK <ahmedkhaledabab@gmail.com>
2018-11-29 18:44:53 +0200

    Heap added

--- src/akopensource/Main.java
+++ src/akopensource/Main.java
@@ -1,8 +1,20 @@
 package akopensource;
 
+import akopensource.maindir.Heap;
+
 public class Main {
 
     public static void main(String[] args) {
         //TESTING
+
+        Integer [] hp = {0,6,5,20,12,30,1,5,5,2,3,4};
+
+        Heap heap = new Heap(hp);
+
+        heap.sort();
+
+        for(int i = 1; i < hp.length; i++){
+            System.out.print(hp[i] + " ");
+        }
     }
 }
--- src/akopensource/maindir/Heap.java
+++ src/akopensource/maindir/Heap.java
@@ -2,4 +2,58 @@ package akopensource.maindir;
 
 public class Heap {
 
+    private Integer [] heap;
+    private int size;
+
+    public Heap(Integer[] heap) {
+        this.heap = heap;
+        this.size = heap.length-1;
+    }
+
+    public void sort() {
+        buildMax();
+        for(int i = heap.length - 1; i >= 2; i--){
+            int temp = heap[1];
+            heap[1] = heap[i];
+            heap[i] = temp;
+            size -= 1;
+            maxHeapify(1);
+        }
+    }
+
+    public void buildMax() {
+        size = heap.length-1;
+        for (int i = heap.length/ 2; i > 1; i--){
+            maxHeapify(i-1);
+        }
+    }
+
+    public void maxHeapify(int i) {
+        int l = left(i);
+        int r = right(i);
+        int largest;
+
+        if(l <= size && heap[l] >= heap[i])
+            largest = l;
+        else largest = i;
+        if(r <= size && heap[r] >= heap[largest])
+            largest = r;
+        if(largest != i) {
+            int temp = heap[i];
+            heap[i] = heap[largest];
+            heap[largest] = temp;
+            maxHeapify(largest);
+        }
+    }
+
+
+
+    public int right(int i) {
+        return 2*i+1;
+    }
+
+    public int left(int i) {
+        return 2*i;
+    }
+
 }
