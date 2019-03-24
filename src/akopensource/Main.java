16c45c4923f9e4cf5360e99f7fde840a3485d9b5
AhmedKhaledAK <ahmedkhaledabab@gmail.com>
2019-03-24 23:02:31 +0200

    generating random arrays

--- src/akopensource/Main.java
+++ src/akopensource/Main.java
@@ -54,7 +54,7 @@ public class Main {
 
         /* HEAP TEST */
 
-        Integer [] hp = {0,6,5,20,12,30,1,5,5,2,3,4};
+        /*Integer [] hp = {0,6,5,20,12,30,1,5,5,2,3,4};
 
         Heap heap = new Heap(hp);
 
@@ -63,7 +63,7 @@ public class Main {
         for(int i = 1; i < hp.length; i++){
             System.out.print(hp[i] + " ");
         }
-
+*/
         /*Quicksort Test */
 
         /*int [] array = {2,8,7,1,3,5,6,4};
