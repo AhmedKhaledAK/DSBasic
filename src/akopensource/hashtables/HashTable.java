f2045e6b95b5da90640d2af70f58407d8eaf8408
AhmedKhaledAK <ahmedkhaledabab@gmail.com>
2019-03-26 16:11:35 +0200

    Hashtable with chaining collision resolving

--- src/akopensource/hashtables/HashTable.java
+++ src/akopensource/hashtables/HashTable.java
@@ -20,6 +20,10 @@ public class HashTable {
         array[index].insertTail(data);
     }
 
+    // Since the linked list is doubly linked list
+    // we can make deletion on O(1) time by sending the node itself to the function
+    // and since we have its pointers, we can delete it easliy in O(1) time.
+
     private int hashIndex(int data) {
         return data % array.length;
     }
