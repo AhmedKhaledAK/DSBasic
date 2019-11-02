df4073a06cd883cba887644cadf70a513514dff3
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-11-02 21:47:33 +0200

    added fibonacci using dynamic programming

--- src/akopensource/DP/DP.java
+++ src/akopensource/DP/DP.java
@@ -2,4 +2,12 @@ package akopensource.DP;
 
 public class DP {
 
+    int fibonacci(int n){
+        int [] fib = new int [n];
+        fib[0]=1;
+        fib[1]=1;
+        for (int i =2; i < n; i++)
+            fib[i] = fib[i-1] + fib[i-2];
+        return fib[n-1];
+    }
 }
