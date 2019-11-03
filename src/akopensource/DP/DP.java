87aa56f323888491c1b8859ab59b084f10745be5
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-11-03 12:17:43 +0200

    added longest common subsequence DP algorithm

--- src/akopensource/DP/DP.java
+++ src/akopensource/DP/DP.java
@@ -15,4 +15,18 @@ public class DP {
         if(n<2) return 1;
         return fibonacciNotDP(n-1) + fibonacciNotDP(n-2);
     }
+
+    public static int LCS(String s1, String s2){
+        int [][] table = new int [s1.length()+1][s2.length()+1];
+        for(int i = 0; i <= s1.length(); i++){
+            for(int j = 0; j <= s2.length(); j++){
+                if(i==0 || j==0) table[i][j] = 0;
+                else if(s1.charAt(i-1) == s1.charAt(j-1)) table[i][j] = 1 + table[i-1][j-1];
+                else table[i][j] = Integer.max(table[i][j-1], table[i-1][j]);
+            }
+        }
+
+        return table[s1.length()][s2.length()];
+    }
+
 }
