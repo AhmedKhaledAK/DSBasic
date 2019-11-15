f59c7c76bb9c55fc17b779bf2c764c8c63a64fba
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-11-15 17:17:21 +0200

    added code of class Node in Huffman

--- src/akopensource/huffman/Node.java
+++ src/akopensource/huffman/Node.java
@@ -1,5 +1,28 @@
 package akopensource.huffman;
 
 public class Node {
+    private char c;
     private int freq;
+    private Node left;
+    private Node right;
+
+    public Node(char c, int freq){
+        this.c = c;
+        this.freq = freq;
+        this.left = null;
+        this.right = null;
+    }
+
+    void addLeft(Node n){
+        this.left = n;
+    }
+
+    void addRight(Node n){
+        this.right = n;
+    }
+
+    int getFreq(){
+        return this.freq;
+    }
+
 }
