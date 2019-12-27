46f4a8396c6567b4e006ddb3f94919ff39a56325
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-12-27 16:20:14 +0200

    added Segment class and function prototypes

---
+++ src/akopensource/geometry/Segment.java
@@ -0,0 +1,16 @@
+package akopensource.geometry;
+
+public class Segment {
+
+    public static boolean doTheyIntersect(Point p1, Point p2, Point p3, Point p4 ){
+        return false;
+    }
+
+    public static int getDirection(Point pi, Point pj, Point pk){
+        return 0;
+    }
+
+    public static boolean onSegment(Point pi, Point pj, Point pk){
+        return false;
+    }
+}
