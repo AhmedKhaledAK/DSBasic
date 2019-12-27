fea6666c3189b86087198b87d6de31a4f2379034
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-12-27 21:00:53 +0200

    written code for doTheyIntersect method

--- src/akopensource/geometry/Segment.java
+++ src/akopensource/geometry/Segment.java
@@ -3,7 +3,25 @@ package akopensource.geometry;
 public class Segment {
 
     public static boolean doTheyIntersect(Point p1, Point p2, Point p3, Point p4 ){
-        return false;
+        double d1 = getDirection(p3,p4,p1);
+        double d2 = getDirection(p3,p4,p2);
+        double d3 = getDirection(p1,p2,p3);
+        double d4 = getDirection(p1,p2,p4);
+
+        if ( ((d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0)) && ((d3 > 0 && d4 < 0) || (d3 < 0 && d4 > 0)) )
+            return true;
+
+        if(d1 == 0 && onSegment(p3,p4,p1))
+            return true;
+
+        if(d2==0 && onSegment(p3,p4,p2))
+            return true;
+
+        if(d3 == 0 && onSegment(p1,p2,p3))
+            return true;
+
+        return d4 == 0 && onSegment(p1, p2, p4);
+
     }
 
     public static double getDirection(Point pi, Point pj, Point pk){
